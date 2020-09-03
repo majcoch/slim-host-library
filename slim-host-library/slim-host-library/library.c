3b51151cfeedcad7f88cee7f73cbd852624b3deb
Michał Granda <majcoch98@gmail.com>
2020-09-03 19:50:13 +0200

    Create empty project

---
+++ slim-host-library/slim-host-library/library.c
@@ -0,0 +1,16 @@
+/*
+ * slim-host-library.c
+ *
+ * Created: 03.09.2020 17:07:04
+ * Author : Micha³ Granda
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ slim-host-library/slim-host-runner/main.c
@@ -0,0 +1,18 @@
+/*
+ * slim-host-runner.c
+ *
+ * Created: 03.09.2020 17:07:24
+ * Author : Micha³ Granda
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
